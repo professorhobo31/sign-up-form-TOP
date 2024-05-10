e04d20e539783d2458b47e79a2a1c3587776a9bb
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-05-10 18:37:39 -0300

    Remove button and replace by working submit button within form element

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Keep the js SIMPLE to add a "Passwords don-t match" warning and functionality to the submit button
+
+let password1 = '';
+let password2 = '';
+
+const submitButton = document.getElementById("submitButton");
+
+function submitForm() {
+    const form = document.getElementById("signUpForm");
+    alert('click')
+}
